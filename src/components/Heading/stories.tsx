06e71c887fae2c7499f2e6eee0996638c95a8777
Mauricio Junqueira <mauvis@gmail.com>
2022-04-13 19:39:21 -0300

    Componente Heading adicionado

---
+++ src/components/Heading/stories.tsx
@@ -0,0 +1,21 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+
+import Heading from '.'
+
+export default {
+  title: 'Heading',
+  component: Heading,
+  argTypes: {
+    children: {
+      type: 'string'
+    }
+  }
+} as ComponentMeta<typeof Heading>
+
+const Template: ComponentStory<typeof Heading> = (args) => <Heading {...args} />
+
+export const Basic = Template.bind({})
+
+Basic.args = {
+  children: 'Most Populars'
+}
---
+++ src/components/Heading/test.tsx
@@ -0,0 +1,35 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+import 'jest-styled-components'
+import Heading from '.'
+
+describe('<Heading />', () => {
+  it('should render a black heading by default', () => {
+    renderWithTheme(<Heading>Won Games</Heading>)
+    expect(screen.getByRole('heading')).toHaveStyle({
+      color: '#030517'
+    })
+  })
+  it('should render a white heading when color is passed', () => {
+    renderWithTheme(<Heading color="white">Won Games</Heading>)
+    expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
+      color: '#FAFAFA'
+    })
+  })
+  it('should render a heading with a line at left', () => {
+    renderWithTheme(<Heading lineLeft>Won Games</Heading>)
+    expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
+      'border-left': '0.7rem solid #3CD3C1'
+    })
+  })
+  it('should render a heading with a line at bottom', () => {
+    renderWithTheme(<Heading lineBottom>Won Games</Heading>)
+    expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyleRule(
+      'border-bottom',
+      '0.5rem solid #F231A5',
+      {
+        modifier: '::after'
+      }
+    )
+  })
+})
